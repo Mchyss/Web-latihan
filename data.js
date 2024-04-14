05e50e79c81cefa432b5bcc8f5dd42f6ff3d4ff9
Mchyss <149641789+Mchyss@users.noreply.github.com>
2024-04-14 19:15:12 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+let usia = 21;
+const nama = "maula";
+
+function generateBiodata(){
+  if(usia === 21){
+    console.log('anda dewasa');
+  } else{
+    console.log('anda anak anak');
+  };
+  
+  console.log(nama, usia);
+}
+
+console.log(nama);
+console.log(usia);
+
+
+generateBiodata();
